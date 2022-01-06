30dcb0b505636a67af2434fbc4696e53b26a1325
Zhaojia-Liang <zliang@csumb.edu>
2022-01-06 06:38:39 -0800

    project added

---
+++ src/main/java/com/demo/spring/mockito/api/dao/UserRepository.java
@@ -0,0 +1,13 @@
+package com.demo.spring.mockito.api.dao;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.demo.spring.mockito.api.model.User;
+
+public interface UserRepository extends MongoRepository<User, Integer>{
+	
+	List<User> findByAddress(String address);
+
+}
